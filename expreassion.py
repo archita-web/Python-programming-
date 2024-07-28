9198e5f270d55c1519c81d03e993db37dc97a764
archita-web <architamishra002@gmail.com>
2024-07-28 19:53:06 +0530

    Add files via upload

---
+++ expreassion.py
@@ -0,0 +1,24 @@
+a,b=2,4
+c = a/b
+print(c)
+a,b=2,4
+c = a//b
+print(c, a/b)
+a,b=2,4
+c = a/b
+print(c)
+a,b=-12,5
+c = a//b
+# number negative 
+print(c)
+
+
+a,b=-5,2
+c = a/b
+print(c)
+a,b=5,2
+c = a/b
+print(c)
+a,b=5,-2
+c = a/b
+print(c)
