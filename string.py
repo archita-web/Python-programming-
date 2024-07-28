cb4eafc0d2190ec129bd558b14b8b1f00d8aec6a
archita-web <architamishra002@gmail.com>
2024-07-28 20:11:38 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,6 @@
+name = 'this is single quote'
+name1= "this is double quote"
+name2 = '''this is triple quote'''
+print(name)
+print(name1)
+print(name2)
---
+++ variable.py
@@ -0,0 +1,13 @@
+name='archita mishra'
+age = 23
+price = 25.9
+a = None
+old = True
+print('my name is',name)
+print('my age is',age)
+print(price)
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(a))
+print(type(old))
