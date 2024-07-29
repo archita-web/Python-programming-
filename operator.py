33770419f3a84f2352ad9c39346603dc9d852e70
archita-web <architamishra002@gmail.com>
2024-07-29 14:37:50 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,33 @@
+#relation Operator
+a = 50
+b = 20
+print(a==b)#false
+print(a!=b)#true
+print(a<=b)#false
+print(a>=b)
+print(a>b)
+print(a<b)
+# Assignment Operator
+num = 10
+num = num + 10
+num += 10
+print('num :', num)
+num -= 10
+print('num :', num)
+num *= 10
+print('num :', num)
+num /= 10
+print('num :', num)
+num %= 10
+print('num :', num)
+num **= 10
+print('num :', num)
+#logical operator
+a = 50
+b = 30 
+print(not False)
+print(not(a>b))
+val1 = True
+val2 = False
+print('And operator', val1 and val2)
+print(' or operator', (a==b) or (a>b))
