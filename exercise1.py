9f05d802fb44d1ebc0ebe24c6e4056673d558206
archita-web <architamishra002@gmail.com>
2024-07-30 10:08:07 +0530

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,12 @@
+num1 = int(input('enter the first number:'))
+num2 = int(input('enter the second number: '))
+sum = num1 + num2
+print('sum: ', sum)
+# WAP to input side of a square & print its area.
+area = num1*num2
+print('area:', area)
+# WAP to input 2 floating point numbers & print their average.
+num3 = float(input('enter'))
+num4 = float(input('enter secoond number'))
+average = (num3 + num4)/2
+print(average)
